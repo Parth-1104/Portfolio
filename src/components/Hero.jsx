a413fec1e9cffc15bec4a7fae8a210854ec72ca0
Parth-1104 <141064865+Parth-1104@users.noreply.github.com>
2025-07-22 00:20:55 +0530

    adda

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,18 @@
 import Spline from '@splinetool/react-spline';
+import Loader from './Loader';
+import React, { useState } from 'react';
 
 export default function Hero() {
+  const [loading, setLoading] = useState(true);
+
   return (
-   
-    <Spline scene="https://prod.spline.design/PfkNpgsR5mMe4k4S/scene.splinecode" />
-   
+    <>
+      {loading && <Loader />}
+      <Spline 
+        scene="https://prod.spline.design/PfkNpgsR5mMe4k4S/scene.splinecode"
+        onLoad={() => setLoading(false)}
+        style={loading ? { display: 'none' } : {}}
+      />
+    </>
   );
 }
